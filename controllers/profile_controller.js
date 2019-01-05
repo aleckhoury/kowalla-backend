cef2169ca3202df439faa7ad8cbe5804e82ae4d9
Tyler O'Briant <tylerobriant@gmail.com>
2019-01-05 16:04:50 -0600

    Project Controller v0 finished
    slight adjustment to profileId handling in project.admins, getting weird self-referential id AND the user id. changing it to a string won't allow us to populate, but this will be fine with our current setup

--- controllers/profile_controller.js
+++ controllers/profile_controller.js
@@ -1,18 +1,34 @@
 // Dependencies
 
 // Models
+const Profile = require('../models/ProfileModel');
 
+/*
+ProfileProps = {
+  name: String,
+  username: String,
+  description: String,
+  profilePicture: String, // TODO: update to have image upload
+  uiColor: String, // string #XXXXXX
+}
+*/
 module.exports = {
   getProfileList(req, res, next) {
     res.send({ function: 'getProfileList' });
   },
 
-  getProfile(req, res, next) {
-    res.send({ function: 'getProfile' });
+  createProfile(req, res, next) {
+    //res.send({ function: 'createProfile' });
+    const profileProps = req.body;
+
+    Profile.create(profileProps)
+      .then(profile => res.send(profile))
+      .catch(err => console.log(err));
+
   },
 
-  createProfile(req, res, next) {
-    res.send({ function: 'createProfile' });
+  getProfile(req, res, next) {
+    res.send({ function: 'getProfile' });
   },
 
   updateProfile(req, res, next) {
--- controllers/project_controller.js
+++ controllers/project_controller.js
@@ -1,25 +1,117 @@
 // Dependencies
 
 // Models
+const Project = require('../models/ProjectModel');
+const Profile = require('../models/ProfileModel');
 
+/*
+1) Create -- first pass done
+2) Delete
+3) Get list
+4) Get specific -- first pass done
+5) update -- first pass done
+*/
+
+/* from the model
+ProjectProps = {
+  name: String,
+  description: String,
+  profilePicture: String, // TODO: change to actual image storage
+  headerPicture: String, // TODO: change to actual image storage
+  admins: [{
+    id: {
+      type: Schema.Types.ObjectId,
+      ref: 'profile'
+    }
+  }],
+}
+*/
+
+async function getProfileIdsFromUsernames(usernames) {
+  let idArray = [];
+
+  for (let i=0; i<usernames.length; i++) {
+    let profileObj = await Profile.findOne({username: usernames[i]});
+    idArray.push(profileObj._id)
+  }
+
+  return idArray;
+}
+
+
+// TODO: add CRUD Headers
 module.exports = {
-  getProjectList(req, res, next) {
-    res.send({ function: 'getProjectList' });
+  async getProjectList(req, res, next) {
+    // Act
+    projects = await Project.find({}); // TODO: Add sorting
+
+    // Send
+    res.send({projects});
   },
 
-  getProject(req, res, next) {
-    res.send({ function: 'getProject' });
+
+  // Create
+  async createProject(req, res, next) {
+    // Init
+    const {
+      name,
+      description,
+      profilePicture,
+      headerPicture,
+      admins,
+    } = req.body;
+
+    // Act
+    const adminIds = await getProfileIdsFromUsernames(admins);
+    const project = await Project.create({
+      name,
+      description,
+      profilePicture,
+      headerPicture,
+      admins: adminIds
+    });
+
+    // Send
+    await project.save();
+    res.status(201).send(project);
   },
 
-  createProject(req, res, next) {
-    res.send({ function: 'createProject' });
+  // Read
+  async getProject(req, res, next) {
+    // Init
+    const { id } = req.params;
+
+    // Act
+    const project = await Project.findOne({_id: id});
+
+    // Send
+    res.status(200).send(project);
   },
 
-  updateProject(req, res, next) {
-    res.send({ function: 'updateProject' });
+  // Update
+  async updateProject(req, res, next) {
+    // Init
+    const { id } = req.params;
+    const updateParams = req.body;
+
+    // Act
+    await Project.findOneAndUpdate({_id: id}, updateParams);
+    const project = await Project.findOne({_id: id});
+
+    // Send
+    res.status(200).send(project);
   },
 
-  deleteProject(req, res, next) {
-    res.send({ function: 'deleteProject' });
+  // Delete
+  async deleteProject(req, res, next) {
+    // Init
+    const { id } = req.params;
+
+    // Act
+    await Project.findOneAndDelete({_id: id});
+    const project = await Project.findOne({_id: id});
+
+    // Send
+    res.status(204).send(project);
   },
 }

925bf79aedc963a9aadc443f5aa0ed756eb0e735
Tyler O'Briant <tylerobriant@gmail.com>
2019-04-02 21:00:45 -0500

    notification model init

--- models/NotificationModel.js
+++ models/NotificationModel.js
@@ -4,10 +4,15 @@ const shortid = require('shortid');
 
 const NotificationSchema = new Schema({
   _id: { type: String, default: shortid.generate },
-  profileId: String,
-  projectId: String,
-  communityId: String,
-  content: String, // TODO: figure out how to store tiptaps
+  ownerProfileId: String, // who is receiving the notification
+  ownerProjectId: String, // who is receiving the notification
+  sendingProfileId: String, // where is the notification coming from? a person interacting
+  sendingProjectId: String, // where is the notification coming from? a project you like
+  sendingCommunityId: String, // where is the notification coming from? for modding
+  postId: String,
+  commentId: String,
+  content: String,
+  type: String,  // subscription notification / interaction / friend did a timed post, etc -> eventually expand into enumerated value
   viewed: Boolean
 },
 {

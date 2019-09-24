69fcc49439cc236d4138f0fe7b33b5dd2a240f4e
Kevin Nuut <kevin@nuut.co>
2019-09-23 23:24:26 -0500

    chore: first dummy jest test

---
+++ __tests__/routes-project.test.js
@@ -0,0 +1,9 @@
+const { request } = require("supertest");
+
+describe("routes/project", () => {
+  describe("getProjectList()", () => {
+    it("success", async () => {
+      expect(true).toBe(true);
+    });
+  });
+});
